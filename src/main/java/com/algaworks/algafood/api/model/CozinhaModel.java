30c35075749a1668a8609ffb1e3086758984d1c6
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-06-27 00:12:12 -0300

    Aula 4.1 até aula 4.22

---
+++ src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.api.model;
+
+import java.util.List;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "cozinhas")
+public class CozinhaModel {
+	
+	@JsonProperty("cozinha")
+	@JacksonXmlElementWrapper(useWrapping = false)
+	private List<Cozinha> cozinhas;
+	
+	public CozinhaModel(List<Cozinha> cozinhas) {
+		super();
+		this.cozinhas = cozinhas;
+	}
+
+	public List<Cozinha> getCozinhas() {
+		return cozinhas;
+	}
+
+	public void setCozinhas(List<Cozinha> cozinhas) {
+		this.cozinhas = cozinhas;
+	}
+	
+}
