1819883ab44c57208d211047ca2e40b20d668ea3
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-06-27 23:31:00 -0300

    Conclusão aula 4

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,13 +1,17 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,7 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -71,6 +77,18 @@ public class RestauranteController {
 		}
     }
 	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarPorCampos(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos){
+		
+		Restaurante restauranteAtual = cadastroRestauranteService.buscar(restauranteId);
+		if (restauranteAtual == null) {
+			ResponseEntity.notFound().build();
+		}
+				
+		merge(campos, restauranteAtual);
+		return atualizar(restauranteId, restauranteAtual);
+	}
+	
 	@DeleteMapping("/{restauranteId}")
 	public ResponseEntity<Restaurante> remover(@PathVariable Long restauranteId){
 		try {
@@ -79,5 +97,20 @@ public class RestauranteController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
 		}		
+	}
+	
+	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+		
+		ObjectMapper objectMapper = new ObjectMapper();	
+		Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
+		
+		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {							
+			Field campo = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			campo.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(campo, restauranteOrigem);
+			
+			ReflectionUtils.setField(campo, restauranteDestino, novoValor);	
+		});
 	}
 }
