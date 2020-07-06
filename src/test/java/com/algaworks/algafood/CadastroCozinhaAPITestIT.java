14da4d4ac0b0f77908fc01a42319c01cda04dccb
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-07-06 10:46:38 -0300

    Aula 10.6 a Aula 10.17 - Concluindo Aula 10

--- src/test/java/com/algaworks/algafood/CadastroCozinhaAPITestIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaAPITestIT.java
@@ -4,11 +4,17 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
+import com.algaworks.algafood.util.ResourceUtils;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -21,12 +27,31 @@ public class CadastroCozinhaAPITestIT {
 	@LocalServerPort
 	private int porta;	
 	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+	
+	private String jsonCozinhaChinesa;
+	Cozinha cozinhaGet;
+	
+	private static final int COZINHA_ID_INEXISTENTE = 999;	
+	
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();//exibe os passos do log no console para ajudar a identificar o erro
 		RestAssured.port = porta;
 		RestAssured.basePath = "/cozinhas";
-
+		
+		jsonCozinhaChinesa = ResourceUtils.getContentFromResource(
+				"/json/correto/cozinha-chinesa.json");
+		
+		databaseCleaner.clearTables();
+		
+		cozinhaGet = new Cozinha();
+		cozinhaGet.setNome("Brasileira");
+		cozinhaGet = cozinhaRepository.save(cozinhaGet);
 	}
 	
 	@Test
@@ -40,21 +65,11 @@ public class CadastroCozinhaAPITestIT {
 	}
 	
 	@Test
-	public void testarRetornoQtdeCozinhasGetCozinha() {
-		RestAssured.given()
-			.accept(ContentType.JSON)
-		.when()
-			.get()
-		.then()
-			.body("", Matchers.hasSize(7))
-			.body("nome", Matchers.hasItems("Brasileira","Italiana"));
-	}
-	
-	@Test
 	public void testarRetornoStatus201PostCozinha() {
 		RestAssured.given()
-			.body("{\"nome\": \"Chinesa\"}")
+			.body(jsonCozinhaChinesa)
 			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
 		.when()
 			.post()
 		.then()
@@ -64,20 +79,20 @@ public class CadastroCozinhaAPITestIT {
 	@Test
 	public void testarRetornoCozinhaGetBuscaCozinhaId(){
 		RestAssured.given()
-			.pathParam("cozinhaId", 2)
+			.pathParam("cozinhaId", cozinhaGet.getId())
 			.accept(ContentType.JSON)
 		.when()
 			.get("/{cozinhaId}")
 		.then()
 			.statusCode(HttpStatus.OK.value())
-			.body("nome", Matchers.equalTo("Americana"));
+			.body("nome", Matchers.equalTo(cozinhaGet.getNome()));
 	}
 	
 
 	@Test
 	public void testarRetornoNotFoundCozinhaGetBuscaCozinhaId(){
 		RestAssured.given()
-			.pathParam("cozinhaId", 999)
+			.pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
 			.accept(ContentType.JSON)
 		.when()
 			.get("/{cozinhaId}")
