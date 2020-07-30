c387145825db31c65ef547c60fd876364c561969
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-07-29 21:18:35 -0300

    Aula 22.18 a 22.17 - Concluindo Aula 20

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -16,7 +16,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()				
 				.anyRequest().authenticated()
 			.and()
-				.oauth2ResourceServer().opaqueToken();
+			.cors().and()
+			.oauth2ResourceServer().opaqueToken();
 	}
 	
 }
