a15ddaafc200e9df35a0a6ec3872b8b0b229d334
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-08-03 08:29:45 -0300

    Aula 23.7 a 23.15

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,22 +1,13 @@
 package com.algaworks.algafood.core.security;
 
-import java.util.Arrays;
-import java.util.List;
-
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
-	private final List<String> COMPLEMENTO_CHAVE = Arrays.asList("a","b","c","d","e","f","g","h","i","j","l","m","n","o","p","q","r","t","u","v","x","z","w","y","k");
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -27,19 +18,5 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.cors().and()
 			.oauth2ResourceServer().jwt();
-	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec(montaChave("mailsonfp").getBytes(), "HmacSHA256");
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
-	
-	private String montaChave(String chaveInicial) {
-		StringBuilder chaveRetorno = new StringBuilder(chaveInicial);
-		
-		COMPLEMENTO_CHAVE.forEach(l -> chaveRetorno.append(l));
-				
-		return chaveRetorno.toString();
 	}
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,9 @@ public class CadastroUsuarioService {
 	@Autowired
 	private CadastroGrupoService cadastroGrupoService;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
     @Transactional
     public Usuario salvar(Usuario usuario) {
     	usuarioRepository.detach(usuario);
@@ -29,6 +33,11 @@ public class CadastroUsuarioService {
     	if(usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
     		throw new NegocioException(String.format("Já existe um usuário cadastrado com o e-mail: %s", usuario.getEmail()));
     	}
+    	
+    	if (usuario.isNovo()) {
+    		usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+    	}
+    	
         return usuarioRepository.save(usuario);
     }
     
@@ -41,9 +50,10 @@ public class CadastroUsuarioService {
     public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
         Usuario usuario = buscarThrow(usuarioId);
         
-        if (usuario.senhaNaoCoincideCom(senhaAtual)) {
+        if (!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
             throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
         }
+        
         
         usuario.setSenha(novaSenha);
     }
